2db728c49ed957f6aa1c426b5b852e5797428689
cyberpunkme <85578224+cyberpunkme@users.noreply.github.com>
2021-06-14 22:11:57 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -2,13 +2,13 @@ import os
 from config import Config
 
 class Translation(object):
-  START_TXT = """<b>Hai {}!!</b>
-<i>I'm Simple Auto file Forward Bot
+  START_TXT = """<i>നമസ്കാരം😸❌️ {}!!</i>
+ <b>എന്റെ പേര് CP AUTO FILE FORWARDER Bot
 This Bot forward all files to One Public channel to Your Personal channel
-More details /help</i>"""
+More details /help</b>"""
   CAPTION = "`{}`\n\n" + str(Config.CAPTION)
   HELP_TXT = """<b>Follow These Steps!!</b>
-<b>• Currectly fill your Heroku Config vars</b> <code>FROM_CHANNEL</code> and <code>TO_CHANNEL</code> <b>and other Vars</b>
+<b>• Correctly fill your Heroku Config vars</b> <code>FROM_CHANNEL</code> and <code>TO_CHANNEL</code> <b>and other Vars</b>
 <b>• Then give admin permission in your personal telegram channel</b>
 <b>• Then send any message In your personal telegram channel</b>
 <b>• Then use /run command in your bot</b>
@@ -21,8 +21,8 @@ More details /help</i>"""
 * /about - <b>About Me</b>"""
   ABOUT_TXT = """<b><u>My Info</b></u>
 
-<b>Name :</b> <code>Auto Forward Bot</code>
-<b>Credit :</b> <code>Dark Angel</code>
+<b>Name :</b> <code>CP FORWARDER Bot</code>
+<b>Creator :</b> <code>@CYBER_PUNK_ME</code>
 <b>Language :</b> <code>Python3</code>
 <b>Lybrary :</b> <code>Pyrogram v1.2.9</code>
 <b>Server :</b> <code>Heroku</code>
